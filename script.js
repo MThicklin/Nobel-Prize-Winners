fec70e1b81f26f741340b87aaba304d9ad44c7b2
YaBoiKentucky <melvinhicklin@gmail.com>
2022-11-16 17:43:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+async function getNPPWinners() {
+    let url = 'https://api.nobelprize.org/2.0/laureates?nobelPrizeYear=' + document.getElementById("year").value;
+    console.log("url: ", url)
+    try {
+        let res = await fetch(url);
+        let result = await res.json();
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderWinners() {
+    let winners = await getNPPWinners();
+    console.log(winners.laureates)
+    winnerCount = winners.laureates.length;
+    document.getElementById("display").innerHTML = "In " + document.getElementById("year").value + " there were " + winnerCount + " winners.";
+    for (x in winners.laureates) {
+        let container = document.createElement("div");
+        container.className = "winner";
+        container.innerHTML = winners.laureates[x].fullName.en + ' - <a href=' + winners.laureates[x].wikipedia.english + '>Wikipedia</a>';
+        document.getElementById("display").appendChild(container);
+    }
+}
---
+++ test.js
@@ -0,0 +1,16 @@
+async function testWinners() {
+    let url = 'https://api.nobelprize.org/2.0/laureates?nobelPrizeYear=' + document.getElementById("year").value;
+    console.log("url: ", url)
+    let res = await fetch(url);
+    let result = await res.json();
+    winnerCount = result.laureates.length;
+    document.getElementById("display").innerHTML = "In " + document.getElementById("year").value + " there were " + winnerCount + " winners.";
+    for (x in result.laureates) {
+        let container = document.createElement("div");
+        container.className = "winner";
+        for (y in result.laureates[x].nobelPrizes) {
+            container.innerHTML = result.laureates[x].fullName.en + ' - ' + <result.laureates[x].nobelPrizes[y] + ' - <a href=' + result.laureates[x].wikipedia.english + '>Wikipedia</a>';
+        document.getElementById("testDisplay").appendChild(container);
+        }
+    }
+}
